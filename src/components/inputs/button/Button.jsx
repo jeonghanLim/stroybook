22c08276610314e1d11d56b83c95a3ec9c1d421a
김수빈 <rolling0321@5finger.co.kr>
2025-02-25 17:13:42 +0900

    버튼 스타일 추가 및 컴포넌트 수정

--- src/components/inputs/button/Button.jsx
+++ src/components/inputs/button/Button.jsx
@@ -3,26 +3,45 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import '@styles/001_theme.css';
+import { typography } from 'storybook/internal/theming';
 
+// 사용자가 선택할 속성들 (사이즈, 타입)
 /** Primary UI component for user interaction */
-export const Button = ({ primary, backgroundColor, size, label, startIcon, endIcon, ...props }) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+export const Button = ({ primary, variant, size, color, label, startIcon, endIcon, disabled, ...props }) => {  
+
+  let sizeClass = "";
+  let colorClass = "";
+  switch(size) {
+    case "sm" : sizeClass = 'btn-size-sm'; break;
+    case "lg" : sizeClass = 'btn-size-lg'; break;
+    default : sizeClass = 'btn-size-md'; break;
+  }
+  switch(color) {
+    case "neutral" : 
+      colorClass = variant === 'contained' ? 'btn-contained-color-neutral' : 
+                    variant === 'outlined' ? 'btn-outlined-color-neutral' : 
+                    'btn-text-color-neutral'; break;
+    default : 
+      colorClass = variant === 'contained' ? 'btn-contained-color-brand' :
+                    variant === 'outlined' ? 'btn-outlined-color-brand' :
+                    'btn-text-color-brand'; break;
+  }
+
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={['btn', sizeClass, colorClass, disabled ? "disabled" : ""].join(' ')}
       {...props}
     >
       <div className="btn-base">
-        <span className="start-icon">{startIcon}</span>
-        <span className="label">{label}</span>
-        <span className="end-icon">{endIcon}</span>
-      </div>
-      <style jsx>{`
-        button {
-          background-color: ${backgroundColor};
+        {startIcon &&
+          <span className="btn-mask-box">{startIcon}</span>
         }
-      `}</style>
+        <span className="label">{label}</span>
+        {endIcon &&
+          <span className="btn-mask-box">{endIcon}</span>
+        }
+      </div>
     </button>
   );
 };
@@ -33,11 +52,16 @@ Button.propTypes = {
   /** What background color to use */
   backgroundColor: PropTypes.string,
   /** How large should the button be? */
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  size: PropTypes.oneOf(['sm', 'md', 'lg']),
+  /** How large should the button be? */
+  variant : PropTypes.oneOf(['contained', 'outlined']),
+  /** How large should the button be? */
+  color : PropTypes.oneOf(['brand', 'neutral']),
   /** Button contents */
   label: PropTypes.string.isRequired,
   /** Optional click handler */
   onClick: PropTypes.func,
+  disabled : PropTypes.bool,
 };
 
 Button.defaultProps = {
