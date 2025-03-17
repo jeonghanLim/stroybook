684636370ebce6bb572357ba9a637220eb9de71f
BOOK-VFFVBU800U\Sehyeon111 <ssehn9327@gmail.com>
2025-03-17 20:55:25 +0900

    css 수정중

--- src/components/inputs/radio/Radio.jsx
+++ src/components/inputs/radio/Radio.jsx
@@ -1,33 +1,34 @@
 import React from "react";
 import PropTypes from 'prop-types';
 import "../../../styles/002system/input.css"
-export default function Radio () {
-    return <div className="radioLabel-size-lg radio-base-fg-brand">
-            <input type="radio" name="color" value="red" id="red" className=""/>
-            <label htmlFor="red">빨강</label>
+export default function Radio() {
+  return <div className="radio-size-md radio-color-brand">
+    <h2 className="radio-title">색을 골라보세요</h2>
+    <input type="radio" name="color" value="red" className="radio-base" />
+    <label htmlFor="red" className="radio-label">빨강</label>
 
-            <input type="radio" name="color" value="blue" id="blue" />
-            <label htmlFor="blue">파랑</label>
+    <input type="radio" name="color" value="blue" className="radio-base" />
+    <label htmlFor="blue" className="radio-label">파랑</label>
 
-            <input type="radio" name="color" value="green" id="green" />
-            <label htmlFor="green">초록</label>
-            </div>;
+    <input type="radio" name="color" value="green" id="green" className="radio-base" />
+    <label htmlFor="green" className="radio-label">초록</label>
+  </div>;
 }
 
 Radio.propTypes = {
   /** How large should the radio be? */
-  color : PropTypes.oneOf(['brand', 'neutral']),
+  color: PropTypes.oneOf(['brand', 'neutral']),
   /** How large should the radio be? */
   size: PropTypes.oneOf(['md', 'lg']),
   /** Optional click handler */
   onClick: PropTypes.func,
 
-  disabled : PropTypes.bool,
+  disabled: PropTypes.bool,
 };
 
-// Radio.defaultProps = {
-//   color: 'brand',
-//   size: 'md',
-//   variant: 'contained',
-//   onClick: undefined,
-// };
+Radio.defaultProps = {
+  color: 'brand',
+  size: 'md',
+  variant: 'contained',
+  onClick: undefined,
+};
