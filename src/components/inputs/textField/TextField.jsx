1018d828643fcd9a731412761656ee3ebd181fda
JungSoy <jungsoyy@gmail.com>
2025-03-23 21:20:54 +0900

    textfield input 제작 중

--- src/components/inputs/textField/TextField.jsx
+++ src/components/inputs/textField/TextField.jsx
@@ -1,23 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const TextField = ({label}) => {
+export const TextField = ({
+  label,
+  dense = false,
+  error = false,
+  disabled = false,
+}) => {
+
+  const classNames = [
+    'textfield-base', 
+    dense && 'dense', 
+    error && 'error', 
+    disabled && 'disabled'
+  ]
+    .filter(Boolean) 
+    .join(' '); 
 
     return (
       <div>
         <div>{label}</div>
-        <div className='textfield-base'>
-          <input type='text' name='test'/>
+        <div className={classNames}>
+            <input type='text' name='test' disabled={disabled}/>
         </div>
       </div>
     )
 }
 
-
 TextField.propTypes = {
-
+  label: PropTypes.string,
+  dense: PropTypes.bool,
+  disabled: PropTypes.bool,
+  error: PropTypes.bool,
 };
 
 TextField.defaultProps = {
-
+  label: '',
+  dense: false,
+  disabled: false,
+  error: false,
 };
