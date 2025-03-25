1d665fd027fa4ad6aeb833138378622f4910a7f8
김수빈 <rolling0321@5finger.co.kr>
2025-03-25 16:11:32 +0900

    수정

--- src/components/dataDisplay/divider/Divider.jsx
+++ src/components/dataDisplay/divider/Divider.jsx
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 /** Primary UI component for user interaction */
 export const Divider = ({ 
   orientation, // 방향
-  fullWidth, // 전체 넓이 100% 설정 여부
+  fullWidth, // 전체 넓이 100% 설정 여부(width: auto로 적용되며, 풀위드를 차지하게 됨)
   component, // 기본 : hr, 추가 설정 가능 : li
   margin, // 좌우(horizontal), 상하(vertical) 마진 설정 가능(0, 2, 4, 8, 12, 16, 20, 24, 32, 40)
   ...props 
@@ -57,7 +57,7 @@ Divider.propTypes = {
   /** HTML element to render? Default 'hr', can also be 'li'. */
   component : PropTypes.string,
   /** What margin class should be applied to customize the divider's margins? */
-  margin : PropTypes.oneOf([0, 2, 4, 8, 12, 16, 20, 24, 32, 40]),
+  margin : PropTypes.oneOf(["0", "2", "4", "8", "12", "16", "20", "24", "32", "40"]),
 };
 
 Divider.defaultProps = {
