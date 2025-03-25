6fa6e7d7d9ce1a5ab1481e996b0218978eae6e44
김수빈 <rolling0321@5finger.co.kr>
2025-03-25 17:54:37 +0900

    divider 조건 수정

--- src/components/dataDisplay/divider/Divider.jsx
+++ src/components/dataDisplay/divider/Divider.jsx
@@ -40,7 +40,7 @@ export const Divider = ({
       className={[
         'divider', 
         orientationClass, 
-        fullWidth ? 'w-auto' : '',
+        (fullWidth && orientation === 'horizontal') ? 'w-auto' : '',
         margin ? `divider-${orientation === 'horizontal' ? 'horizontal' : 'vertical'}-${margin}` : ''
       ].filter(Boolean).join(' ')} 
       {...props} 
