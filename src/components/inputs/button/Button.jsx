f18a22e878635bde7f4d1d95a448a1b701a3d62d
임정한 <wjdgks4752@gmail.com>
2025-03-26 13:23:02 +0900

    버튼 props 형태 정리

--- src/components/inputs/button/Button.jsx
+++ src/components/inputs/button/Button.jsx
@@ -2,62 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import useRipple from '@/hooks/useRipple';
 
-// 사용자가 선택할 속성들 (사이즈, 타입)
-/** Primary UI component for user interaction */
-export const Button = ({ 
-  variant= 'contained',
-  size= 'md',
-  increasedWidth = false,
-  increasedHeight = false,
-  color= 'brand',
-  disabled = false,
-  label, 
-  startIcon, 
-  endIcon, 
-  onClick,
-  ...props 
-}) => {  
-  
-  const handleClick = (e) => {
-    if(disabled) return;
-    onClick && onClick(e);
-  }
-
-  const { handleFocus, handleBlur, rippleContainer} = useRipple({centered: false, color : variant == 'contained' ? 'white' : color});
-
-  return (
-    <button
-      type="button"
-      className={`btn btn-size-${size} btn-${variant}-color-${color} ${disabled && disabled}`}
-      onClick={handleClick}
-      onFocus={handleFocus}
-      onBlur={handleBlur}
-      style={{width: increasedWidth && '100%', height: increasedHeight && '100%'}}
-      {...props}
-    >
-      <div className="btn-base">
-        {startIcon &&
-          <span className="btn-mask-box">{startIcon}</span>
-        }
-        {label && 
-          <span className="label">
-            {label}
-          </span>
-        }
-        {endIcon &&
-          <span className="btn-mask-box">{endIcon}</span>
-        }
-      </div>
-      {rippleContainer}
-    </button>
-  );
-};
-
 Button.propTypes = {
   variant : PropTypes.oneOf(['contained', 'outlined', 'text']),
   size: PropTypes.oneOf(['sm', 'md', 'lg']),
   increasedWidth  : PropTypes.bool,
-  increasedHeight  : PropTypes.bool,
   color : PropTypes.oneOf(['brand', 'neutral']),
   label: PropTypes.string,
   startIcon : PropTypes.element, 
@@ -70,7 +18,82 @@ Button.defaultProps = {
   variant: 'contained',
   size: 'md',
   increasedWidth : false,
-  increasedHeight : false,
   color: 'brand',
   disabled : false,
 };
+
+export const Button = ({ 
+  variant
+  , size
+  , increasedWidth
+  , color
+  , disabled
+  , label
+  , startIcon
+  , endIcon
+  , onClick
+  , ...props 
+}) => {  
+  
+  // code
+
+  const handleClick = (e) => {
+    if(disabled) {
+      e.preventDefault();
+      return;
+    }
+    onClick && onClick(e);
+  }
+
+  const rippleOption = {
+    centered: false
+    , color : variant == 'contained' ? 'white' : color
+  }
+
+  const { 
+    handleFocus
+    , handleBlur
+    , rippleContainer 
+  } = useRipple(rippleOption);
+
+  return (
+    <button
+      type="button"
+      className={`btn btn-size-${size} btn-${variant}-color-${color} ${disabled && disabled}`}
+      onClick={handleClick}
+      onFocus={!disabled && handleFocus}
+      onBlur={!disabled && handleBlur}
+      style={{width: increasedWidth && '100%'}}
+      {...props}
+    >
+      <div className="btn-base">
+        {startIcon &&
+          <span className="btn-mask-box">{startIcon}</span>
+        }
+        {label && 
+          <span className="label">{label}</span>
+        }
+        {endIcon &&
+          <span className="btn-mask-box">{endIcon}</span>
+        }
+      </div>
+      {rippleContainer}
+    </button>
+  );
+};
+
+/**
+  className
+
+  btn-size-sm
+  btn-size-md
+  btn-size-lg
+
+  btn-contained-color-brand
+  btn-outlined-color-brand
+  btn-text-color-brand
+
+  btn-contained-color-neutral
+  btn-outlined-color-neutral
+  btn-text-color-neutral
+ */
