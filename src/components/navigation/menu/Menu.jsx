5ef1bb17520e6029dc4258a0bd63acb79db482df
김수빈 <rolling0321@5finger.co.kr>
2025-03-28 14:09:45 +0900

    css 속성 작성 중

--- src/components/navigation/menu/Menu.jsx
+++ src/components/navigation/menu/Menu.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Divider } from '@/components/dataDisplay/divider/Divider';
 
 export const Menu = ({
-  startIcon
+  fullWidth = false // 전체 넓이 100% 설정 여부
+  , dense = false // compact한 레이아웃 설정 여부
+  , disableGutter = false // 좌우 패딩 삭제 여부
+  , maxHeight // 최대 높이 적용(높이에 따라 스크롤바 적용됨)
+  , startIcon
   , endIcon
-  , label
-  , menuItemDivider
-  , ...props 
+  , label = "Menu" // 메뉴아이템 라벨 텍스트
+  , disabled = false // 비활성화
+  , ...props
 }) => {  
 
     return (
@@ -24,7 +27,6 @@ export const Menu = ({
               }
             </div>
           </div>
-          { menuItemDivider && <Divider /> }
         </div>
       </div>
     );
@@ -32,8 +34,22 @@ export const Menu = ({
 
 
 Menu.propTypes = {
-  /** Menu 앞쪽  */
+  /** Menu 전체 넓이 100% 설정 여부 */
+  fullWidth: PropTypes.bool,
+  /** Menu 컴팩트한 레이아웃 설정 여부 */
+  dense: PropTypes.bool,
+  /** Menu 좌우 패딩 삭제 여부 */
+  disableGutter: PropTypes.bool,
+  /** Menu 최대 높이 적용 */
+  maxHeight: PropTypes.string,
+  /** MenuItem 앞쪽 아이콘 설정 */
+  startIcon: PropTypes.element,
+  /** MenuItem 뒷쪽 아이콘 설정 */
+  endIcon: PropTypes.element,
+  /** MenuItem 라벨(텍스트) */
+  label: PropTypes.string,
 };
 
 Menu.defaultProps = {
+  label: "Menu",
 };
